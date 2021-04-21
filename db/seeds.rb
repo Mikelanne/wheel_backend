5cb0bbda6195acf9e841f8138079eefa318d4ddc
Mikelanne <mikelanne.wood@gmail.com>
2021-04-21 12:33:48 -0400

    Adding Location seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# rand = Character.new(name: "Rand Al'Thor", ta_veren: true, abilities: "Uses the One Power. Has control of Callandor, a sword made of the One Power.", title: "The Dragon Reborn" home: "The Two Rivers", group_id: )
+
+Location.new(name: "The Two Rivers", leader: "Perin Aybara", description: "The Two Rivers is a region in Andor, marked with two rivers on each side; The Taren River and the White River. The Two Rivers contains the villiages of Taren Ferry, Deven Ride, Edmond's Field, and Watch Hill."
+Location.new(name: "White Tower", leader: "The Amyrlin Seat", description: "The White Tower is the central structure inside the city of Tar Valon that houses the Aes Sedai and Warders. Aes Sedai and Warders are both trained within the grounds of the White Towwer.")
+Location.new(name: "Tar Valon", leader: "None", description: "Tar Valon is an island in the River Erinin. The White Tower is located within Tar Valon.")
+Location.new(name: "Edmond's Field", leader: "Queen of Andor", description: "Edmond's Field is a villiage in the Two Rivers in the district of Andor.")
+Location.new(name: "Taren Ferry", leader: "Queen of Andor", description: "Taren Ferry lies on the norther boundary of the Two Rivers.")
+Location.new(name: "Deven Ride", leader: "Queen of Andor", description: "Devon Ride is located near the southern boundary of the Two Rivers.")
+Location.new(name: "Watch Hill", leader: "Queen of Andor", description: "Watch Hill is in the north region of the Two Rivers.")
+Location.new(name: "Arad Doman", leader: "Alsalam Saeed Almadar", description: "Arad Doman is located in the Westlands on the Aryth Ocean. Arad Doman has a long standing rivalry with Tarabon.")
+Location.new(name: "Tarabon", leader: "Panarch and King", description: "Tarabon is located on the southwest coast of the Westlands. Tanchico is the capital.")
+Location.new(name: "Tanchico", leader: "Panarch and King", description: "Tanchico is one of the oldest cities in the world and the capital of Tarabon.")
+Location.new(name: "Malkier", leader: "None", description: "Malkier was a nation in the Boarderlands until 955 NE. Malkier is now overrun by the Great Blight.")
+Location.new(name: "Saldaea", leader: "Queen Tenobia", description: "Saldaea is one of the nations in the Boarderlands.")
+Location.new(name: "Shienar", leader: "King Easar Togita", description: "Shienar is one of the nations in the Boarderlands. Previously bordered on Malkier.")
+Location.new(name: "Cairhien", leader: "The Sun Throne", description: "Cairhien is a nation in the Westlands. Known as the Tree Killers by the Aiel, Cairhien once had a prosperous trade through the Aiel Waste. The Guild of the Illuminators is located here.")
+Location.new(name: "Illian", leader: "King", description: "Illian is a nation in the Westlands, on the Sea of Storms. Illian and Tear are long-standing enemies.")
+Location.new(name: "Tear", leader: "Rand Al'Thor", description: "Tear is a nation in the Westlands. Tear boarders the Aiel Waste and the Sea of Storms. Famous for its fortress, The Stone of Tear, which was overthrown for the first time in several thousand years by Rand Al'Thor.")
+Location.new(name: "The Stone of Tear", leader: "Rand Al'Thor", description: "The Stone of Tear is a massive fortress located in Tear. It was built by the Aes Sedai with the One Power. The Stone protects and houses Callandor.")
+Location.new(name: "Mayene", leader: "The First", description: "Mayene is a small nation on the Sea of Storms. Mayene is completely surrounded by Tear.")
+Location.new(name: "The Aiel Waste AKA The Three Fold Land", leader: "12 Clan Chiefs.", description: "The Aiel Waste lies to the east of the Spine of the World. This is where the Aiel live.")
+Location.new(name: "Falme", leader: "", description: "Falme is the largest city on Toman Head, located on the tip of Toman Head. A battle between The Dark One and Rand Al'Thor was displayed in the sky here.")
+Location.new(name: "Baerlon", leader: "Queen of Andor", description: "Baerlon is located in western Andor.")
+
+
