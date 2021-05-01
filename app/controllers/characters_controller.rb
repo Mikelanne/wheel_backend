5c39d68dc6136c98b28ca4da0487ce055d33f893
Mikelanne <mikelanne.wood@gmail.com>
2021-05-01 15:24:55 -0400

    cleaning up group_id for new form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,4 +9,20 @@ class CharactersController < ApplicationController
         character = Character.find(params[:id])
         render json: CharacterSerializer.new(character)
     end
+
+
+    def create
+        character = Character.new(character_params)
+        if character.save
+            render json: CharacterSerializer.new(character)
+        else
+            render json: {message: "unable to save character"}
+        end
+    end
+
+    private
+
+    def character_params
+        params.require(:character).permit(:name, :title, :ta_veren, :home, :abilities, :group_id, :image)
+    end
 end
