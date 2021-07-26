c70e4d5c3df1f7dd05f85758a0b5736114c46d65
Mikelanne <mikelanne.wood@gmail.com>
2021-07-26 12:24:26 -0400

    decided the changes I want to make require a new repo

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-    has_many :characters
-    has_many :groups, through: :characters
-    has_many :locations
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_182029) do
+ActiveRecord::Schema.define(version: 2021_04_23_170322) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2021_07_25_182029) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
-    t.integer "user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -37,14 +36,6 @@ ActiveRecord::Schema.define(version: 2021_07_25_182029) do
     t.string "name"
     t.string "leader"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
